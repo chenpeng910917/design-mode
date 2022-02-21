23f360c3e5292e76455b5345e44d2f6f15c02fe4
陈鹏 <917chenpeng>
2022-02-21 16:35:29 +0800

    设计模式

--- src/main/java/com/design/mode/singleton/SingletonDemo.java
+++ src/main/java/com/design/mode/singleton/SingletonDemo.java
@@ -6,6 +6,18 @@ package com.design.mode.singleton;
  * 1、单例类只能有一个实例。
  * 2、单例类必须自己创建自己的唯一实例。
  * 3、单例类必须给所有其他对象提供这一实例。
+ * 反射方式会破坏掉单例
+ *
+ * 应用场景：idGenerate生成器、Log日志对象等
+ * 问题：为什么数据库连接池不设计为单例模式 答：有多个连接池时不能扩展
+ *
+ * 单例缺点:
+ *      单例对 OOP 特性的支持不友好
+ *      单例会隐藏类之间的依赖关系
+ *      单例对代码的扩展性不友好
+ *      单例对代码的可测试性不友好
+ *      单例不支持有参数的构造函数
+ * 解决方案：使用工厂模式、ioc容器
  *
  * @author chenpeng
  * @date 2022/2/15
@@ -16,10 +28,17 @@ public class SingletonDemo {
      * 在启动jvm参数中添加 -XX:+TraceClassLoading 查看加载情况
      */
     public static void main(String[] args) {
-        // 饿汉模式
+        // 饿汉模式 建议使用
         hungryMan();
-        // 饿汉静态内部类模式
+        // 饿汉静态内部类模式 明确要求需要延迟加载
         hungryManStaticInner();
+        // 懒汉模式 不建议使用
+        slacker();
+        // 双重检查
+        doubleCheck();
+        // 枚举模式
+        singletonEnum();
+
     }
 
     public static void hungryMan() {
@@ -39,5 +58,17 @@ public class SingletonDemo {
         // 会加载HungryManSingleton 打印HungryManSingleton静态代码块中的日志
         System.out.println(HungryManStaticInnerSingleton.CONSTANT);
         HungryManStaticInnerSingleton singleton = HungryManStaticInnerSingleton.getSingleton();
+    }
+
+    public static void slacker() {
+        SlackerSingleton instance = SlackerSingleton.getInstance();
+    }
+
+    public static void doubleCheck() {
+        DoubleCheckSingleton singleton = DoubleCheckSingleton.getSingleton();
+    }
+
+    public static void singletonEnum() {
+        SingletonEnum instance = SingletonEnum.INSTANCE;
     }
 }
---
+++ src/main/java/com/design/mode/singleton/SingletonEnum.java
@@ -0,0 +1,28 @@
+package com.design.mode.singleton;
+
+/**
+ * 枚举单例
+ * 虚拟机保证的只创建一次 通过查询字节码看原理 创建了一个静态的INSTANCE
+ *
+ * 编译项目
+ * cd target/classes/com/design/mode/singleton
+ * javap -c SingletonEnum.class 查看字节码  javap -v SingletonEnum.class 更详细
+ *
+ * @author chenpeng
+ */
+
+public enum SingletonEnum {
+    /**
+     * 实例
+     */
+    INSTANCE();
+
+    static {
+        System.out.println("加载枚举单例");
+    }
+
+    public void get() {
+        System.out.println("枚举");
+    }
+
+}
