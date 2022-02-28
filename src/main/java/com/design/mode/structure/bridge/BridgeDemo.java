67dded0b5b8b8bcb0ad1ee2f0861778026041a5a
陈鹏 <917chenpeng>
2022-02-28 17:14:30 +0800

    设计模式

---
+++ src/main/java/com/design/mode/structure/bridge/BridgeDemo.java
@@ -0,0 +1,30 @@
+package com.design.mode.structure.bridge;
+
+import com.design.mode.structure.bridge.notify.NormalNotification;
+import com.design.mode.structure.bridge.notify.ServerNotification;
+import com.design.mode.structure.bridge.notify.UrgencyNotification;
+import com.design.mode.structure.bridge.send.DingDingMsgSender;
+import com.design.mode.structure.bridge.send.TelephoneMsgSender;
+
+/**
+ * 桥接模式例子
+ *
+ * @author chenpeng
+ * @date 2022/2/28
+ */
+public class BridgeDemo {
+    public static void main(String[] args) {
+
+        // 钉钉服务通知发送消息
+        ServerNotification serverNotification = new ServerNotification(new DingDingMsgSender());
+        serverNotification.notify("钉钉服务通知发送消息");
+
+        // 钉钉紧急通知发送消息
+        UrgencyNotification urgencyNotification = new UrgencyNotification(new DingDingMsgSender());
+        urgencyNotification.notify("钉钉紧急通知发送消息");
+
+        // 手机短信正常通知发送消息
+        NormalNotification normalNotification = new NormalNotification(new TelephoneMsgSender());
+        normalNotification.notify("手机短信正常通知发送消息");
+    }
+}
---
+++ src/main/java/com/design/mode/structure/bridge/notify/UrgencyNotification.java
@@ -0,0 +1,22 @@
+package com.design.mode.structure.bridge.notify;
+
+import com.design.mode.structure.bridge.send.MsgSender;
+
+/**
+ * 紧急通知
+ *
+ * @author chenpeng
+ */
+public class UrgencyNotification extends Notification {
+
+    public UrgencyNotification(MsgSender msgSender) {
+        super(msgSender);
+    }
+
+    @Override
+    public void notify(String message) {
+        //在此处可以实现业务紧急通知的具体业务逻辑
+
+        msgSender.send(message);
+    }
+}
